147495b00d8f0c60f2f59b76534da05fd7f14fe2
Veng-Eang <oengvengeang@gmail.com>
2023-01-25 09:03:31 +0700

    add new ASP.NET M3 year 4

---
+++ MvcMovie/Controllers/ARController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class ARController : Controller
+    {
+        public IActionResult M3()
+        {
+            return View();
+        }
+        public IActionResult ReturnSpecifiedView()
+        {
+            return View("M Three");
+        }
+        public ActionResult ControllerPassDataToView(int id,string name)
+        {
+            ViewData["StuID"] = id;//store data in ViewData
+            ViewData["StuName"]=name;
+
+            ViewBag.StuID = id;//store data in Viewbag
+            ViewBag.StuName = name;
+            return View("Data");
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action....";
+        }
+
+
+        public string Welcome()
+        {
+            return "This is the welcome action method.....";
+        }
+        public string Introduction (int ID,string Name)
+        {
+            return "I'm Mr. " + Name + " .My ID is " + ID;
+        }
+        /*
+         * https://localhost:7100/helloworld/introduction?id=99&name=veng
+        */
+    }
+}
